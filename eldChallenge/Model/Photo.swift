61e8c235be3ecf4db4cd1f08679abc0eeed804e6
Robson James <james.robson.junior@gmail.com>
2020-01-15 00:37:58 -0400

    Created Photo Model to Decodable and persistence model to CoreData

---
+++ eldChallenge/Model/Photo.swift
@@ -0,0 +1,26 @@
+//
+//  Photo.swift
+//  eldChallenge
+//
+//  Created by Robson James Junior on 14/01/20.
+//  Copyright © 2020 robsonJames. All rights reserved.
+//
+
+import UIKit
+
+class Photo: Decodable {
+  public let title: String
+  public let url: String
+  public let thumbnailUrl: String
+  
+  enum CodingKeys: String, CodingKey {
+    case title, url, thumbnailUrl
+  }
+  
+  required init(from decoder: Decoder) throws {
+      let container = try decoder.container(keyedBy: CodingKeys.self)
+      title = try container.decode(String.self, forKey: .title)
+      url = try container.decode(String.self, forKey: .url)
+      thumbnailUrl = try container.decode(String.self, forKey: .thumbnailUrl)
+  }
+}
